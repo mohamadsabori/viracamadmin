bfee74b0d68ce6a62b4b64de7a29d8a6dcd20328
mohamadsabori <m.sabori.86@gmail.com>
2018-03-25 01:39:41 +0430

    new

--- src/app/model/Product.ts
+++ src/app/model/Product.ts
@@ -1,14 +1,22 @@
-export class Product {
-  private id: number;
-  public name: String;
-  public cost: String;
-  public description: String;
+import {ProductImages} from "./productImages";
+import {ProductProperties} from "./productProperties";
+import {ProductCategory} from "./productCategory";
+/**
+ * Created by Mohamad on 3/22/2018.
+ */
+export class Product{
+  name: string;
+  cost: string;
+  description: string;
+  productCode: string;
+  id: number;
+  productImages: Array<ProductImages>;
+  productProperties: Array<ProductProperties>;
+  category: ProductCategory;
 
-  constructor(private nameParam: String, private costParam: String, private descriptionParam: String) {
-    this.id = 0;
-    this.name = nameParam;
-    this.cost = costParam;
-    this.description = descriptionParam;
+  constructor() {
+  this.productImages = new Array();
+  this.productProperties = new Array();
+  this.category = new ProductCategory();
   }
-
 }
